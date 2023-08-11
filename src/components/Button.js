adc6c62f320ebf8d302f900e43a81947fba51291
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-08-11 10:00:15 +0100

    Update js files

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ onClick, className, value }) {
-  const handleClick = () => {
+function Button({ className, value, onClick }) {
+  const handleButtonClick = () => {
     onClick(value);
   };
 
   return (
-    <button type="button" className={className}>
+    <button type="button" className={className} onClick={handleButtonClick}>
       {value}
     </button>
   );
 }
 
 export default Button;
+
 Button.propTypes = {
-  onClick: PropTypes.func.isRequired,
   className: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
+
+// function Button({ className, value, onClick }) {
+//   return (
+//     <button type="button" className={className} onClick={() => onClick(value)}>
+//       {value}
+//     </button>
+//   );
+// }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,18 +1,25 @@
 /*eslint-disable*/
 import React, { useState } from 'react';
-// import * as math from 'mathjs';
 import Button from './Button';
 import Result from './Result';
 import calculate from '../logic/calculate';
 
 function Calculator() {
-const handleButtonClick = (e) => {
-  console.log(e.target);
-}
+  const [calculatorData, setCalculatorData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleButtonClick = (value) => {
+    const newCalculatorData = calculate(calculatorData, value);
+    setCalculatorData(newCalculatorData);
+  };
+
   return (
     <div className="parent">
-      <Result value={0} />
-      {/* <Button onClick={handleButtonClick} className="white-btn" value="AC" />
+      <Result value={calculatorData.next || calculatorData.total || '0'} />
+      <Button onClick={handleButtonClick} className="white-btn" value="AC" />
       <Button onClick={handleButtonClick} className="white-btn" value="+/-" />
       <Button onClick={handleButtonClick} className="white-btn" value="%" />
       <Button onClick={handleButtonClick} className="orange-btn" value="/" />
@@ -24,15 +31,15 @@ const handleButtonClick = (e) => {
       <Button onClick={handleButtonClick} className="white-btn" value="5" />
       <Button onClick={handleButtonClick} className="white-btn" value="6" />
       <Button onClick={handleButtonClick} className="orange-btn" value="-" />
-      <Button onClick={handleButtonClick} className="white-btn" value="1" /> */}
-      <Button onClick={(e) => handleButtonClick(e)} className="white-btn" value="2" />
-      {/* <Button onClick={handleButtonClick} className="white-btn" value="3" />
+      <Button onClick={handleButtonClick} className="white-btn" value="1" />
+      <Button onClick={handleButtonClick} className="white-btn" value="2" />
+      <Button onClick={handleButtonClick} className="white-btn" value="3" />
       <Button onClick={handleButtonClick} className="orange-btn" value="+" />
       <Button onClick={handleButtonClick} className="white-btn zero" value="0" />
       <Button onClick={handleButtonClick} className="white-btn" value="." />
-      <Button onClick={handleButtonClick} className="orange-btn" value="=" /> */}
+      <Button onClick={handleButtonClick} className="orange-btn" value="=" />
     </div>
   );
-};
+}
 
 export default Calculator;
