ff80ce5ef80007e1710274f1e2921fecefa44610
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-08-15 18:51:39 +0100

    Create Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => (
         tellus ullamcorper consectetur id ac odio. Phasellus convallis a lacus
         eget semper. Interdum et malesuada fames ac ante ipsum primis in faucibus.
         Aenean vestibulum nibh ac dolor malesuada varius. Morbi nec posuere magna.
-        Morbi bibendum eros vel ligula tempus, sit amet cursus libero aliquam. Duis
+        Morbi bibendum eros vel ligula tempus, sit amet curus libero aliquam. Duis
         vestibulum eu quam vel lacinia. Pellentesque vel mollis nisl, in feugiat risus.
         Aliquam erat volutpat. Suspendisse id ullamcorper lectus, eget consequat elit.
       </p>
