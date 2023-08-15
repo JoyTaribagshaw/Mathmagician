77b09d35f454c62e2494b0c017eba190f321efdf
Joy Tari-bagshaw <111312788+JoyTaribagshaw@users.noreply.github.com>
2023-08-15 18:50:57 +0100

    Update Calculator component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <div className="home_page">
+      <h1 className="home_heading">Welcome to Math Magicians!</h1>
+      <p className="home_paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus metus
+        augue, mattis vel iaculis non, fringilla vitae orci. Cras eu lectus non
+        tellus ullamcorper consectetur id ac odio. Phasellus convallis a lacus
+        eget semper. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+        Aenean vestibulum nibh ac dolor malesuada varius. Morbi nec posuere magna.
+        Morbi bibendum eros vel ligula tempus, sit amet cursus libero aliquam. Duis
+        vestibulum eu quam vel lacinia. Pellentesque vel mollis nisl, in feugiat risus.
+        Aliquam erat volutpat. Suspendisse id ullamcorper lectus, eget consequat elit.
+      </p>
+    </div>
+  </div>
+);
+
+export default Home;
